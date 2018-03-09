5b5ee84e7423dab3a55e0da33674b4041824a23a
vanAmsterdam <woutervanamsterdam@gmail.com>
2018-03-09 11:18:31 +0100

    added dispersion function

--- R/stats.R
+++ R/stats.R
@@ -1,3 +1,51 @@
+#' Calculate dispersion of a model fit
+#'
+#' Calculate dispersion of glm model fit
+#'
+#' @author Wouter van Amsterdam
+#' @export
+#' @param fit the result of a glm fit
+#' @return a lenght 1 numeric with the dispersion factor
+
+dispersion <- function(fit) {
+  p_resids = residuals(fit, type = "pearson")
+  df_resid = fit$df.resid
+  return(sum(p_resids^2)/df_resid)
+}
+
+
+#' Get proportion and confidence interval from a logical variable
+#'
+#' Get proportion and confidence interval from a logical variable in one line
+#'
+#' @author Wouter van Amsterdam
+#' @export
+#' @param x a logical variable (or 0-1 numeric / integer)
+#' @param prop_true logical flag, get proportion of 'trues' or 'falses'
+#' @param method passed to \code{\link{binom::binom.confint}} for method
+#' of interval calculation. Default is 'wilson'
+#' @param conf.level confidence level for interval
+#' @param ... other options for \code{\link{binom::binom.confint}}
+#' @return a \code{data.frame} with 1 row containing
+#' number of trues, length of vector, proportion (mean), lower and upper bound for intervals.
+
+binom.confint_logical <- function(x, prop_true = T,
+                                  method = "wilson", conf.level = 0.95, ...) {
+  if (!(is.logical(x) | setequal(unique(x), c(0, 1)))) {
+    stop("provide logical vector or vector with 0-1 values")
+  }
+
+  if (!is.logical(x)) x = as.logical(x)
+
+  binom::binom.confint(
+    x = ifelse(prop_true, sum(x), sum(!x)),
+    n = length(x),
+    conf.level = conf.level,
+    method = method,
+    ...
+  )
+}
+
 
 #' Sorted data.frame of correlations
 #'
